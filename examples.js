fb15ea7450bb931dfd91629dc03f0deff46423a9
Olena Gapak <6997804+OlenaGapak@users.noreply.github.com>
2023-12-19 17:05:51 +0100

    separated css and examples. updated index

---
+++ examples.js
@@ -0,0 +1,17 @@
+var texts = [
+    {
+        text: "Тестовий запис 1 Російська держава створює альянс пам'яті з сербськими політичними елітами, експортуючи не тільки відповідні меседжі, але й артефакти, як-от георгіївську стрічку. the export of Russian practices, including the Saint George Ribbon and the Immortal Regiment procession, which have been adopted with enthusiasm by Serbian elites, who wear them during parades.  McGlynn, J. (2020)",
+        categories: {
+            "cat 1": "option 1",
+            "cat 2": "option 3",
+            "cat 3": "option 3",
+        }
+    },
+    {
+        text: "Тестовий запис 2",
+        categories: {
+            "cat 1": "option 1",
+            "cat 3": "option 1",
+        }
+    },
+]
